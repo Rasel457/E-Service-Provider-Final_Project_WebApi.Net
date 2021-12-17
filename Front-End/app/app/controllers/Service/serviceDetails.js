71f5c7eb9f5b8f50609787b52fc7ad759edcd5ef
Rasel457 <rasel.aiub990a2gmail.com>
2021-12-17 22:24:34 +0600

    Add comment done

--- Front-End/app/app/controllers/Service/serviceDetails.js
+++ Front-End/app/app/controllers/Service/serviceDetails.js
@@ -1,10 +1,32 @@
-app.controller("serviceDetails",function($scope,ajax,$routeParams){
+app.controller("serviceDetails",function($scope,ajax,$routeParams,$location){
     ajax.get("api/CustomerService/WithComment/"+$routeParams.id,success,error);
     function success(response){
         $scope.s=response.data;
     }
     function error(error){
-       console.log(error);
+        console.log(error);
     }
+   
+
+    $scope.submit=function(){
+        var data = {
+                    service_id:$routeParams.id,
+                    comment1=$scope.comment1,
+                    user_id:9
+              
+                 };
+        //console.log(data);
+        ajax.post("api/CustomerHome/AddComment", data, success, error)
+        function success(response){
+        console.log(response);
+        $location.path("/CustomerHome/AllService");
+            alert("Comment send Successfully");
+        }
+        function error(error){
+            console.log(error);
+            alert("Failed To Added !!!!");
+        }
+     }          
+    
 
 });
