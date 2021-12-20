30edea455d8afa0abe7ec643f95fddae47d9d73b
Rasel457 <rasel.aiub990a2gmail.com>
2021-12-21 02:08:34 +0600

    Log in done

--- Front-End/app/app/controllers/Customer/CustomerLogin.js
+++ Front-End/app/app/controllers/Customer/CustomerLogin.js
@@ -2,7 +2,11 @@ app.controller("CustomerLogin",function($scope,$http,ajax,$location){
     $scope.login=function(){
         ajax.post("api/login",$scope.data,success,error);
         function success(response){
-            console.log(response.data);
+            //console.log(response.data);
+            if(response==null)
+            {
+                $scope.invalid="Invalid Email or Password";
+            }
             localStorage.setItem("UserId",response.data.UserId);
             localStorage.setItem("token",response.data.AccessToken);
             $location.path("/CustomerHome");
